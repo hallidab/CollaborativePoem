926cf666052a8791abc5f3226e54908c049503c5
Davidson <davidslm@egb070000032.lancs.local>
2020-02-06 12:53:40 +0000

    My poem

--- poem.java
+++ poem.java
@@ -2,6 +2,6 @@ public class poem
 {
     public static void main( String[]arguments)
     {
-        System.out.println("Hello world");
+        System.out.println("Hello world\nThis is a poem \nWithin a Java build \nI don't know where I'm goin' \n ");
     }
 }
